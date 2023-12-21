76e4b89ebb3432eaa15b1f084f167a4d2dcc6b17
Oleksandr Chervonniy <sheph@ukr.net>
2023-12-21 21:40:02 +0100

    added font 'Rubik'

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,30 +5,20 @@ export const Navbar = () => {
   return (
     <nav className={s.nav}>
       <ul>
-        <li>
-          <a href="#" className={s.item}>
-            Profile
-          </a>
+        <li className={s.item}>
+          <a href="#">Profile</a>
         </li>
-        <li>
-          <a href="#" className={s.item}>
-            Messages
-          </a>
+        <li className={s.item}>
+          <a href="#">Messages</a>
         </li>
-        <li>
-          <a href="#" className={s.item}>
-            News
-          </a>
+        <li className={s.item}>
+          <a href="#">News</a>
         </li>
-        <li>
-          <a href="#" className={s.item}>
-            Music
-          </a>
+        <li className={s.item}>
+          <a href="#">Music</a>
         </li>
-        <li>
-          <a href="#" className={s.item}>
-            Settings
-          </a>
+        <li className={s.item}>
+          <a href="#">Settings</a>
         </li>
       </ul>
     </nav>
