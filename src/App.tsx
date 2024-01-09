9a95382bc1f3493aaffd96f4d84f98f79dddab17
Oleksandr Chervonniy <sheph@ukr.net>
2024-01-09 15:58:50 +0100

    add a Dialog section

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,19 @@ import { Header } from "./components/Header/Header";
 import { Navbar } from "./components/Navbar/Navbar";
 import { Profile } from "./components/Profile/Profile";
 import { Footer } from "./components/Footer/Footer";
+import { Dialogs } from "./components/Dialogs/Dialogs";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <div className="content-wrapper cont">
+      <main className="main cont">
         <Navbar />
-        <Profile />
-      </div>
+        <div className="content-main">
+          {/*<Profile />*/}
+          <Dialogs />
+        </div>
+      </main>
       <Footer />
     </div>
   );
---
+++ src/components/Dialogs/Dialogs.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import s from "./Dialogs.module.scss";
+
+export const Dialogs = () => {
+  return (
+    <section className={"dialog"}>
+      <p>Dialog window</p>
+    </section>
+  );
+};
